c8c8f4dc2ee9884a24c90201a2ae1e2272b1bad6
alejandro-priet <alejandro.prieto@pm.me>
2021-08-20 13:01:11 +0200

    Initial deploy

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
+from models import Feedback, db
+
+app = Flask(__name__)
+
+ENV = 'production'
+
+if ENV == 'development':
+    POSTGRES = {
+        'user': 'postgres',
+        'pw': 'a2dejlnor9054',
+        'db': 'mvpdb',
+        'host': 'localhost',
+        'port': '5432',
+    }
+    app.config['DEBUG'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+    %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+else :
+    app.config['DEBUG'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yssnhbufmhvkze:8fef3ac8f13467f5123dfa014bcafabc29d5d4113df3c775135277865fb850d8@ec2-34-194-14-176.compute-1.amazonaws.com:5432/d24dr5l6kfgcbh'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        service = request.form['service']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer, dealer, rating, comments)
+        if customer == '' or service == '' or rating == '' or comments == '':
+            return render_template('index.html', message='Please fill in all fields')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, service, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, service, rating, comments)
+            return render_template('success.html')
+        return render_template('index.html', message='You are alreasy registered')
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+
+class BaseModel(db.Model):
+    """Base data model for all objects"""
+    __abstract__ = True
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def __repr__(self):
+        """Define a base way to print models"""
+        return '%s(%s)' % (self.__class__.__name__, {
+            column: value
+            for column, value in self._to_dict().items()
+        })
+
+    def json(self):
+        """
+                Define a base way to jsonify models, dealing with datetime objects
+        """
+        return {
+            column: value if not isinstance(value, datetime.date) else value.strftime('%Y-%m-%d')
+            for column, value in self._to_dict().items()
+        }
+
+
+class Feedback(BaseModel, db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments

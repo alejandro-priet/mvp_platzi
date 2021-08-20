24ae42cae696d33e40ff4be8d9f25e6a337e533d
alejandro-priet <alejandro.prieto@pm.me>
2021-08-20 13:22:39 +0200

    Initial deploy

--- app.py
+++ app.py
@@ -2,28 +2,43 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from send_mail import send_mail
 from models import Feedback, db
+from flask_migrate import Migrate
+from flask_script import Manager
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-ENV = 'production'
+def create_app():
 
-if ENV == 'development':
-    POSTGRES = {
-        'user': 'postgres',
-        'pw': 'a2dejlnor9054',
-        'db': 'mvpdb',
-        'host': 'localhost',
-        'port': '5432',
-    }
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-    %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-else :
-    app.config['DEBUG'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yssnhbufmhvkze:8fef3ac8f13467f5123dfa014bcafabc29d5d4113df3c775135277865fb850d8@ec2-34-194-14-176.compute-1.amazonaws.com:5432/d24dr5l6kfgcbh'
+    app = Flask(__name__)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+    ENV = 'production'
+
+    if ENV == 'development':
+        POSTGRES = {
+            'user': 'postgres',
+            'pw': 'a2dejlnor9054',
+            'db': 'mvpdb',
+            'host': 'localhost',
+            'port': '5432',
+        }
+        app.config['DEBUG'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+        %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    else :
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yssnhbufmhvkze:8fef3ac8f13467f5123dfa014bcafabc29d5d4113df3c775135277865fb850d8@ec2-34-194-14-176.compute-1.amazonaws.com:5432/d24dr5l6kfgcbh'
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+    db.app = app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return app
+
+app = create_app()
 
 
 @app.route('/')

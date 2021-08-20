362701d7fb8479d3deafeb81bd931dc6801e228b
alejandro-priet <alejandro.prieto@pm.me>
2021-08-20 15:29:02 +0200

    Initial deploy

--- models.py
+++ models.py
@@ -36,4 +36,8 @@ class Feedback(BaseModel, db.Model):
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments

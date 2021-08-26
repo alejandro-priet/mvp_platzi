b391e3b014aa7d9750ebf4a6e053d5fcce459ab7
alejandro-priet <alejandro.prieto@pm.me>
2021-08-26 16:26:59 +0200

    finishing basic registration loging and logout interfaces and logic

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
